e066d4f0b35d3c109aefe19d8e8f945e219efadd
bramvonk <git@bramvonk.nl>
2025-11-09 12:09:41 +0100

    Spring Initializer with "Spring Data JPA" and "H2"

---
+++ src/main/java/com/example/column_naming_spring_boot_4_issue/ColumnNamingSpringBoot4IssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.column_naming_spring_boot_4_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumnNamingSpringBoot4IssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColumnNamingSpringBoot4IssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/column_naming_spring_boot_4_issue/ColumnNamingSpringBoot4IssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.column_naming_spring_boot_4_issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColumnNamingSpringBoot4IssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
